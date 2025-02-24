c20751a8acd1f8aaff14a5e5736450ce7fb98afd
vnritb <victor.naranjo@itb.cat>
2025-02-24 19:26:19 +0100

    Modificat el punt d'entrada

--- composeApp/src/commonMain/kotlin/cat/itb/m78/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/App.kt
@@ -7,12 +7,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cat.itb.m78.exercices.theme.AppTheme
+import cat.itb.m78.p2.samples.squa
 import dev.mateuy.teaching.compose.resumen.CounterApp2
 
 @Composable
 internal fun App() = AppTheme {
     Box(Modifier.fillMaxSize()){
 //        Text("Your app goes here!!!", Modifier.align(Alignment.Center))
-        CounterApp2()
+        //CounterApp2()
+        squa()
     }
 }
--- composeApp/src/commonMain/kotlin/cat/itb/m78/p2/samples/square.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/p2/samples/square.kt
@@ -1,13 +1,15 @@
 package cat.itb.m78.p2.samples
 
+import androidx.compose.runtime.Composable
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 @Serializable
 data class Rectangle(val height: Double, val width: Double)
 
-fun main() {
-
+@Composable
+fun squa() {
+//fun main() {
     //val rectangle = Rectangle(42.3, 22.2)
     val rectangle = Json.decodeFromString<Rectangle>("""{"width":42.2, "height": 33.2}""")
     val rectangleList = listOf(rectangle)
